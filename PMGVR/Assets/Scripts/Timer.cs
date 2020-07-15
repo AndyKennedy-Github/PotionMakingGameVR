5fe0e02fb05982d768e7b97241e356232009f295
AndyKennedy-Github <61878684+AndyKennedy-Github@users.noreply.github.com>
2020-07-15 12:30:39 -0400

    Added Timer Logic to Game Manager

---
+++ PMGVR/Assets/Scripts/Timer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timeInRound = 120;
+    public bool startTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(startTime)
+        {
+            timeInRound -= Time.deltaTime;
+            if (timeInRound < 0)
+            {
+                return;
+            }
+        }
+    }
+}
