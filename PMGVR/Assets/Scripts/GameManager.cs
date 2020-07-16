9eade58e30ed9623ea4fad43582067f253041e1b
AndyKennedy-Github <61878684+AndyKennedy-Github@users.noreply.github.com>
2020-07-16 13:48:20 -0400

    Fleshed out Game Manager Script
    Created a singleton Game Manager which will hold game values like total stars, total gold, in level gold and stars, and also started developing the coroutines for specific tasks like the end of level tally and whatnot.

--- PMGVR/Assets/Scripts/GameManager.cs
+++ PMGVR/Assets/Scripts/GameManager.cs
@@ -4,15 +4,83 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+
+    int totalGameGold, levelGold, levelGoldGoal, totalGameStars, levelStars;
+    int firstStarGoal, secondStarGoal, thirdStarGoal;
+
+    public bool inLevel, inMap, levelEnded;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(inLevel)
+        {
+            StarCount();
+        }
+
+        /*if(levelEnded == false)
+         {
+            EndOfLevel();
+            levelEnded = true;
+         }
+         */
         
+    }
+
+    public void ResetLevelGoldandStars()
+    {
+        levelGold = 0;
+        levelStars = 0;
+    }
+
+    public void SetLevelGoal(int i)
+    {
+        levelGoldGoal = i;
+    }
+
+    void StarCount()
+    {
+        if(levelGold > firstStarGoal)
+        {
+            //display first star achieved!
+            levelStars = 1;
+        }
+        else if(levelGold > secondStarGoal)
+        {
+            //display second star achieved!
+            levelStars = 2;
+        }
+        else if(levelGold > thirdStarGoal)
+        {
+            //display third star achieved!
+            levelStars = 3;
+        }
+    }
+
+    IEnumerator EndOfLevel()
+    {
+        totalGameGold += levelGold;
+        totalGameStars += levelStars;
+        yield return null;
     }
 }
